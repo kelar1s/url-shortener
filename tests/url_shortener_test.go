9324f287739a955bdb8027d2822906f3d7931811
Anton <atabalaev21@gmail.com>
2025-09-21 22:05:59 +0300

    added tests for url-shortener

---
+++ tests/url_shortener_test.go
@@ -0,0 +1,104 @@
+package tests
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/kelar1s/url-shortener/internal/http-server/handlers/url/save"
+	"github.com/kelar1s/url-shortener/internal/lib/random"
+)
+
+const (
+	host = "localhost:8082"
+)
+
+func TestURLShortener_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	e.POST("/url").
+		WithJSON(save.Request{
+			URL:   gofakeit.URL(),
+			Alias: random.NewRandomString(10),
+		}).
+		WithBasicAuth("myuser", "mypass").
+		Expect().
+		Status(200).
+		JSON().Object().
+		ContainsKey("alias")
+}
+
+func TestURLShortener_SaveRedirect(t *testing.T) {
+	testCases := []struct {
+		name  string
+		url   string
+		alias string
+		error string
+	}{
+		{
+			name:  "Valid URL",
+			url:   gofakeit.URL(),
+			alias: gofakeit.Word() + gofakeit.Word(),
+		},
+		{
+			name:  "Invalid URL",
+			url:   "invalid_url",
+			alias: gofakeit.Word(),
+			error: "field URL is not a valid URL",
+		},
+		{
+			name:  "Empty Alias",
+			url:   gofakeit.URL(),
+			alias: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			u := url.URL{
+				Scheme: "http",
+				Host:   host,
+			}
+
+			e := httpexpect.Default(t, u.String())
+
+			resp := e.POST("/url").
+				WithJSON(save.Request{
+					URL:   tc.url,
+					Alias: tc.alias,
+				}).
+				WithBasicAuth("myuser", "mypass").
+				Expect().Status(http.StatusOK).
+				JSON().Object()
+
+			if tc.error != "" {
+				resp.NotContainsKey("alias")
+				resp.Value("error").String().IsEqual(tc.error)
+				return
+			}
+
+			alias := tc.alias
+			if tc.alias != "" {
+				resp.Value("alias").String().IsEqual(tc.alias)
+			} else {
+				resp.Value("alias").String().NotEmpty()
+				alias = resp.Value("alias").String().Raw()
+			}
+
+			redirectResp := e.GET("/"+alias).
+				WithBasicAuth("myuser", "mypass").
+				WithRedirectPolicy(httpexpect.DontFollowRedirects).
+				Expect().
+				Status(http.StatusFound).
+				Headers()
+
+			redirectResp.HasValue("Location", []string{tc.url})
+		})
+	}
+}
